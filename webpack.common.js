7269ede08cfb1279d4c7afe0c0fce47c07b71839
Sajid <sajidahamed2001@gmail.com>
2024-12-01 01:24:28 +0530

    template finalized

--- webpack.common.js
+++ webpack.common.js
@@ -23,6 +23,10 @@ const config = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
   },
 };
