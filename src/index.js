28ef0eb5dd65b13b819f0dcf7ca63cb9a776747f
Sajid <sajidahamed2001@gmail.com>
2024-12-03 11:23:14 +0530

    page layout

--- src/index.js
+++ src/index.js
@@ -2,3 +2,22 @@ import "./styles.css"
 console.log('test succefulll');
 
 console.log('ping pong');
+
+const temp = document.querySelector('.xxyy')
+console.log(temp);
+
+
+function run(){
+    console.log('runnig fast');
+    dialog.showModal()
+}
+
+const addbutton = document.querySelector('.addbutton')
+
+addbutton.addEventListener('click',run)
+
+const dialog = document.querySelector("dialog")
+
+const submit = document.querySelector('.submit')
+submit.addEventListener('click', run)
+
