9616c91ac1df1528a272662dde6e277b998a8e7d
Sajid <sajidahamed2001@gmail.com>
2024-12-18 20:24:13 +0530

    taskIdCounter is stored and retrieved from local storage

--- src/index.js
+++ src/index.js
@@ -10,36 +10,37 @@ document.addEventListener("DOMContentLoaded", taskListeners);
 document.addEventListener("DOMContentLoaded", projectEventListeners);
 
 export let tasksArray = [];
-
+export function isArrayEmpty(arr) {
+  return arr.length === 0;
+}
 if (localStorage.getItem("tasksArray")) {
-//   console.log(true);
-
   tasksArray = JSON.parse(localStorage.getItem("tasksArray"));
-  renderAllTasks()
-} else console.log('no tasks found');
+  console.log(isArrayEmpty(tasksArray));
+  renderAllTasks();
+} else console.log("no tasks found");
 
 //since tasksArray can only be modified in this module
 //this function is used to modifiy the tasksArray from other modules
 //this function is called anywhere we have to change the value of tasksArra
 //with the argument of temporary array then the tasksArray is changed
-//one example is in taskManager.js in deletebuttonHandler where deleted 
+//one example is in taskManager.js in deletebuttonHandler where deleted
 //tasks are removed from the array the filtered or deleted items are removed
-//and the filgered array is temporarily stored in a array and this function is 
+//and the filgered array is temporarily stored in a array and this function is
 //called to update the value in tasksArray
-export function updateTasksArray(newArray){
-    tasksArray = newArray
-    //and after any change to the tasksArray it is save to the localStorage
-    localStorage.setItem('tasksArray',JSON.stringify(tasksArray))
+export function updateTasksArray(newArray) {
+  tasksArray = newArray;
+  //and after any change to the tasksArray it is save to the localStorage
+  localStorage.setItem("tasksArray", JSON.stringify(tasksArray));
 }
 
-export let projectsArray = []
-if (localStorage.getItem('projectsArray')) {
-    // console.log(true);
-    projectsArray = JSON.parse(localStorage.getItem('projectsArray'))
-    renderAllProjects()
-}else console.log('no projects found');
-export function updateProjectsArray(newArray){
-    projectsArray = newArray
+export let projectsArray = [];
+if (localStorage.getItem("projectsArray")) {
+  // console.log(true);
+  projectsArray = JSON.parse(localStorage.getItem("projectsArray"));
+  renderAllProjects();
+}
+export function updateProjectsArray(newArray) {
+  projectsArray = newArray;
 
-    localStorage.setItem('projectsArray',JSON.stringify(projectsArray))
+  localStorage.setItem("projectsArray", JSON.stringify(projectsArray));
 }
--- src/taskManager.js
+++ src/taskManager.js
@@ -1,42 +1,56 @@
-import {  renderTask } from "./taskRenderer";
-import { tasksArray ,updateTasksArray } from ".";
-
+import { renderTask } from "./taskRenderer";
+import { tasksArray, updateTasksArray } from ".";
+import { isArrayEmpty } from ".";
 
 //  let tasksArray = intiTasksArray()
 // export let tasksArray = []
 export function formHandling() {
-    const title = document.getElementById("taskTitle").value;
-    const description = document.getElementById("taskDescription").value;
-    const dueDate = document.getElementById("taskDueDate").value;
-    const selectedProjectId_string = document.querySelector('select[name="project"').value
+  const title = document.getElementById("taskTitle").value;
+  const description = document.getElementById("taskDescription").value;
+  const dueDate = document.getElementById("taskDueDate").value;
+  const selectedProjectId_string = document.querySelector(
+    'select[name="project"'
+  ).value;
 
-    const selectedProjectId = parseInt(selectedProjectId_string)
+  const selectedProjectId = parseInt(selectedProjectId_string);
 
+  const taskObject = createTaskObject(
+    title,
+    description,
+    dueDate,
+    selectedProjectId
+  );
 
-    const taskObject = createTaskObject(title, description, dueDate,selectedProjectId);
-
-    tasksArray.push(taskObject);
-    updateTasksArray(tasksArray)
-    renderTask(taskObject);
+  tasksArray.push(taskObject);
+  updateTasksArray(tasksArray);
+  renderTask(taskObject);
 }
-let taskIdCounter = 0;
-function createTaskObject(title, description, dueDate,projectId) {
-    return {
-        id: taskIdCounter++,
-        title: title,
-        description: description,
-        dueDate: dueDate,
-        projectId:projectId,
-    
-        completed: false,
-    };
+let taskIdCounter;
+function createTaskObject(title, description, dueDate, projectId) {
+  if (isArrayEmpty(tasksArray)) {
+    taskIdCounter = 0;
+    console.log(taskIdCounter);
+  } else {
+    taskIdCounter = localStorage.getItem("taskIdCounter", taskIdCounter);
+  }
+  let task = {
+    id: taskIdCounter++,
+    title: title,
+    description: description,
+    dueDate: dueDate,
+    projectId: projectId,
+
+    completed: false,
+  };
+  localStorage.setItem("taskIdCounter", taskIdCounter);
+  return task;
 }
 
 export function deleteButtonHandler(button, taskId) {
-    const filteredArray = tasksArray.filter((tasks) => tasks.id !== taskId);
-    updateTasksArray(filteredArray)
-    const noteDiv = button.closest(".notes");
-    if (noteDiv) {
-        noteDiv.remove();
-    }
+  const filteredArray = tasksArray.filter((tasks) => tasks.id !== taskId);
+  updateTasksArray(filteredArray);
+  const noteDiv = button.closest(".notes");
+  if (noteDiv) {
+    noteDiv.remove();
+  }
 }
